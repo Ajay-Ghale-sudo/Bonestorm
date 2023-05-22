c221740cf0cc5b8dd45413cebe9bdbb899d23751
Tyler <tyler@pseudocube.com>
2023-05-21 20:25:47 -0700

    Added flying enemy. Added flying dragon enemy

---
+++ Source/BoomerShooter/Public/Character/Enemy/BsDragon.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Character/Enemy/BsFlyingEnemy.h"
+#include "BsDragon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOOMERSHOOTER_API ABsDragon : public ABsFlyingEnemy
+{
+	GENERATED_BODY()
+	
+};

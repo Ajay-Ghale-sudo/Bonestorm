0268fb0c76ad30ba75dba3decbf459d9d6c445fc
Andrew Karjala <andrewkarj@gmail.com>
2023-05-29 16:42:23 -0700

    Created enemy spawner class

---
+++ Source/BoomerShooter/Private/Spawner/BsEnemySpawner.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spawner/BsEnemySpawner.h"
+
+
+// Sets default values
+ABsEnemySpawner::ABsEnemySpawner()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABsEnemySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABsEnemySpawner::Spawn()
+{
+	UWorld* World = GetWorld();
+	if (SpawnedEnemy && World)
+	{
+	}
+}
+
+// Called every frame
+void ABsEnemySpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+

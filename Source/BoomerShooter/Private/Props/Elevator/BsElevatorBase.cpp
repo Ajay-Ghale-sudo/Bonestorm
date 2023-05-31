b78f22baf5054319cf6b52fd0cb09a9b5a125a2c
Andrew Karjala <andrewkarj@gmail.com>
2023-05-31 15:32:11 -0700

    Added float curve to elevators, need to add static mesh and test

---
+++ Source/BoomerShooter/Private/Props/Elevator/BsElevatorBase.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Props/Elevator/BsElevatorBase.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+ABsElevatorBase::ABsElevatorBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	SetRootComponent(CapsuleComponent);
+	ElevatorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ElevatorMesh"));
+	ElevatorMesh->SetupAttachment(CapsuleComponent);
+	ElevatorEndComponent = CreateDefaultSubobject<USceneComponent>(TEXT("ElevatorEnd"));
+	ElevatorEndComponent->SetupAttachment(CapsuleComponent);
+}
+
+
+// Called every frame
+void ABsElevatorBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MoveToLocation(DeltaTime);
+}
+
+void ABsElevatorBase::Interact(AActor* Interactor)
+{
+	
+}
+
+// Called when the game starts or when spawned
+void ABsElevatorBase::BeginPlay()
+{
+	Super::BeginPlay();
+	LerpStartLocation = ElevatorMesh->GetRelativeLocation();
+	LerpEndLocation = ElevatorEndComponent->GetRelativeLocation();
+}
+
+void ABsElevatorBase::Activated()
+{
+	bIsActivated = true;
+}
+
+void ABsElevatorBase::MoveToLocation(float DeltaTime)
+{
+	if (!bIsActivated && ElevatorFloatCurve) return;
+	UWorld* World = GetWorld();
+	//ElevatorFloatCurve->FloatCurve.GetLastKey();
+	// Elevator is going upwards
+	LerpEndLocation = ElevatorEndComponent->GetComponentLocation();
+	//FMath::Lerp(LerpStartLocation, LerpEndLocation
+}
+
+
---
+++ Source/BoomerShooter/Public/Props/Elevator/BsElevatorBase.h
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Interfaces/Interactable.h"
+#include "BsElevatorBase.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS()
+class BOOMERSHOOTER_API ABsElevatorBase : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABsElevatorBase();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Interact(AActor* Interactor) override;
+
+	/**
+	 * @brief Generic function that activates the elevator.
+	 **/
+	UFUNCTION(BlueprintCallable, meta = (AllowInEventReceiver = "true"))
+	void Activated();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void MoveToLocation(float DeltaTime);
+protected:
+	
+	/**
+	 * @brief Static mesh of the elevator. Set in blueprint.
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* ElevatorMesh;
+
+	/**
+	 * @brief Capsule component of the elevator
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UCapsuleComponent* CapsuleComponent;
+	
+	/**
+	 * @brief End location of the elevators lerp destination
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USceneComponent* ElevatorEndComponent;
+	
+	/**
+	 * @brief Float curve the Lerp follows. Set in blueprint.
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UCurveFloat* ElevatorFloatCurve;
+	
+	/**
+	 * @brief Start location for the elevator to lerp from
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector LerpStartLocation;
+
+	/**
+	 * @brief End location for the elevator to lerp to
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FVector LerpEndLocation;
+	
+	/**
+	 * @brief Set if elevator is activated
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bIsActivated = false;
+
+	/**
+	 * @brief Set if the elevator is going up
+	 **/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	bool bGoingUp = true;
+};

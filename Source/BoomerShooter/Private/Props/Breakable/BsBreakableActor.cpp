587c642cdcdc656d3d74026968e2e387b0172d1b
Tyler <tyler@pseudocube.com>
2023-07-05 14:33:53 -0700

    Added initial breakable glass object. Added M_Glass material

---
+++ Source/BoomerShooter/Private/Props/Breakable/BsBreakableActor.cpp
@@ -0,0 +1,30 @@
+﻿#include "Props/Breakable/BsBreakableActor.h"
+#include "Component/BsHealthComponent.h"
+
+// Sets default values
+ABsBreakableActor::ABsBreakableActor()
+{
+	// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	SetRootComponent(MeshComponent);
+
+	HealthComponent = CreateDefaultSubobject<UBsHealthComponent>("HealthComponent");
+}
+
+// Called when the game starts or when spawned
+void ABsBreakableActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HealthComponent)
+	{
+		HealthComponent->OnDeath.AddDynamic(this, &ABsBreakableActor::Break);
+	}
+}
+
+void ABsBreakableActor::Break()
+{
+	OnBreak.Broadcast();
+}
